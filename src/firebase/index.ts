5d10f05c376664e83c2850975e2f5589de57e039
Heinrich Francis <heinrich@ubuntux.co.za>
2026-02-18 10:17:02 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/con

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,17 @@
 import { initializeApp, getApp, getApps, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
+
+// The Firebase configuration is now directly in this file to prevent loading issues.
+export const firebaseConfig = {
+  "projectId": "studio-845965156-c3a3b",
+  "appId": "1:1014429404657:web:fe6855f3d2ed43d89bc850",
+  "storageBucket": "studio-845965156-c3a3b.appspot.com",
+  "apiKey": "AIzaSyBkP1hVPRjxoeuY9mRa7XU-0lZH5jdWzQo",
+  "authDomain": "studio-845965156-c3a3b.firebaseapp.com",
+  "messagingSenderId": "1014429404657",
+  "measurementId": ""
+};
 
 export function initializeFirebase(): { app: FirebaseApp; auth: Auth; firestore: Firestore } {
   const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
