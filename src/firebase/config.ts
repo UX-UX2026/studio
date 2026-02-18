0ca87646ac6c6731bd15b86b037feb16046193ea
Heinrich Francis <heinrich@ubuntux.co.za>
2026-02-18 07:23:21 +0000

    add this key: AIzaSyBkP1hVPRjxoeuY9mRa7XU-0lZH5jdWzQo

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,4 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be automatically injected by the Firebase CLI during deployment.
 // Running the app locally will use the demo project configuration.
-export const firebaseConfig = {"apiKey":"your-api-key","authDomain":"your-project-id.firebaseapp.com","projectId":"your-project-id","storageBucket":"your-project-id.appspot.com","messagingSenderId":"your-sender-id","appId":"your-app-id"};
+export const firebaseConfig = {"apiKey":"AIzaSyBkP1hVPRjxoeuY9mRa7XU-0lZH5jdWzQo","authDomain":"your-project-id.firebaseapp.com","projectId":"your-project-id","storageBucket":"your-project-id.appspot.com","messagingSenderId":"your-sender-id","appId":"your-app-id"};
