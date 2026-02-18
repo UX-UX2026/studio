7851f4e6f5eb0ea9e078f5223f3bc0d0fbb2b40d
Heinrich Francis <heinrich@ubuntux.co.za>
2026-02-18 07:39:18 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,11 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be automatically injected by the Firebase CLI during deployment.
 // Running the app locally will use the demo project configuration.
-export const firebaseConfig = {"apiKey":"AIzaSyBkP1hVPRjxoeuY9mRa7XU-0lZH5jdWzQo","authDomain":"your-project-id.firebaseapp.com","projectId":"your-project-id","storageBucket":"your-project-id.appspot.com","messagingSenderId":"your-sender-id","appId":"your-app-id"};
+export const firebaseConfig = {
+  "projectId": "studio-845965156-c3a3b",
+  "appId": "1:1014429404657:web:fe6855f3d2ed43d89bc850",
+  "storageBucket": "studio-845965156-c3a3b.appspot.com",
+  "apiKey": "AIzaSyBkP1hVPRjxoeuY9mRa7XU-0lZH5jdWzQo",
+  "authDomain": "studio-845965156-c3a3b.firebaseapp.com",
+  "messagingSenderId": "1014429404657"
+};
