772d56f8a452019ad79b5bfcf27602c6990623bc
Heinrich Francis <heinrich@ubuntux.co.za>
2026-02-20 07:25:09 +0000

    can we tel the next.js to skip these folders

--- src/app/(dashboard old)/settings/roles/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is intentionally disabled to prevent build errors.
-// It is part of a deprecated route group and should not be processed.
-export default function DisabledPage() {
-  return null;
-}
