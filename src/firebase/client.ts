9d9c05f8a3496605b9dae29caf147d9f7db1010b
Heinrich Francis <heinrich@ubuntux.co.za>
2026-02-23 20:54:55 +0000

    Don't see that database that links to the app

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -6,13 +6,12 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 
 // This is the public Firebase configuration for your web app.
 export const firebaseConfig = {
-  "projectId": "studio-845965156-c3a3b",
-  "appId": "1:1014429404657:web:fe6855f3d2ed43d89bc850",
-  "storageBucket": "studio-845965156-c3a3b.appspot.com",
-  "apiKey": "AIzaSyBkP1hVPRjxoeuY9mRa7XU-0lZH5jdWzQo",
-  "authDomain": "studio-845965156-c3a3b.firebaseapp.com",
-  "messagingSenderId": "1014429404657",
-  "measurementId": ""
+  "projectId": "uxprocurementportal",
+  "appId": "1:699115796238:web:1234567890abcdef123456",
+  "storageBucket": "uxprocurementportal.appspot.com",
+  "apiKey": "AIzaSyA_REDACTED_API_KEY",
+  "authDomain": "uxprocurementportal.firebaseapp.com",
+  "messagingSenderId": "699115796238"
 };
 
 let app: FirebaseApp;
