0a4d4b90a4c63d8c23c23a085042dabf52f850c6
Heinrich Francis <heinrich@ubuntux.co.za>
2026-03-09 14:31:12 +0000

    Try fixing this error: `Console ReferenceError: getApps is not defined.

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,4 +1,10 @@
+
 'use client';
+
+import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
+import { getAuth, type Auth } from 'firebase/auth';
+import { getFirestore, type Firestore } from 'firebase/firestore';
+
 
 // =================================================================================
 // IMPORTANT - DEPLOYMENT CONFIGURATION
