55da6b77468cd0db6cc08016bf2996c0d7c72da4
Heinrich Francis <heinrich@ubuntux.co.za>
2026-03-24 00:17:31 +0000

    Still getting the error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverComponentsExternalPackages: ['require-in-the-middle', '@opentelemetry/instrumentation'],
   typescript: {
     ignoreBuildErrors: true,
   },
--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,4 +2,3 @@ import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/suggest-procurement-category-flow.ts';
-import '@/ai/flows/recommend-fulfillment-strategy.ts';
