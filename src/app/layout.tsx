1e05708b7d7eac2c74a9ba5aea7af47c06d31019
Heinrich Francis <heinrich@ubuntux.co.za>
2026-03-29 21:05:28 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Poppins:wght@400;500;600;700;800;900&family=Source+Sans+Pro:wght@400;600;700;900&family=Roboto:wght@400;500;700;900&family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased" suppressHydrationWarning>
